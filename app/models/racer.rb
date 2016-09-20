d582fcf02ed42ba9f62bc8109be7bec89410a443
Chris Turner <mr.chris.turner@gmail.com>
2016-09-19 17:38:57 -0700

    finished the pagination specs

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -79,13 +79,13 @@ def self.paginate (params)
 	page=(params[:page] || 1).to_i
 	limit=(params[:per_page] || 30).to_i
 	skip=(page-1)*limit
-	
+	sort={:number=>1}
 
 	racers=[]
-		all(params, :_id,skip, limit).each do |doc|
-		racers << Racer.new(doc)
+		all({}, sort, skip, limit).each do |doc|
+		  racers << Racer.new(doc)
 	end
-		total=all(params, 0, 1).count
+		total=all().count
 
 	WillPaginate::Collection.create(page, limit, total) do |pager|
 		pager.replace(racers)
